d42b5c0a33bcf0aaf8ab8865d83377d543c7aad6
khaled hammami <khaled.hammami740@gmail.com>
2024-01-12 12:09:12 +0100

    feat: add admin functionality and simple dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@
 import Navbar from "@/components/navbar"
 import { useSession } from "@clerk/nextjs"
 
-export default function Home() {
 
+export default function Home() {
   return (
     <main className="min-w-full ">
       <div className="flex w-full justify-center">welcome to next app </div>
